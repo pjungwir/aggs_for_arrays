90da1758c41b8c9d7c0a3fb39ef4e2188aed580f
Paul A. Jungwirth <pj@illuminatedcomputing.com>
2014-12-11 22:11:04 -0800

    Added array_to_mean tests and fixed a bug in the computation

--- Makefile
+++ Makefile
@@ -8,7 +8,8 @@ include $(PGXS)
 
 test:
 	./test/setup.sh
-	PATH="./test/bats:$$PATH" ./test/bats/bats test/array_to_hist.sh
+	# PATH="./test/bats:$$PATH" bats test/array_to_hist.bats test/array_to_mean.bats
+	PATH="./test/bats:$$PATH" bats test
 
 bench:
 	./bench/setup.sh
--- array_to_mean.c
+++ array_to_mean.c
@@ -11,9 +11,6 @@ array_to_mean(PG_FUNCTION_ARGS)
 {
   // Our arguments:
   ArrayType *vals;
-  pgnum bucketsStart;
-  pgnum bucketsSize;
-  int32 bucketsCount;
 
   // The array element type:
   Oid valsType;
@@ -36,7 +33,7 @@ array_to_mean(PG_FUNCTION_ARGS)
   // The size of the input array:
   int valsLength;
 
-  pgnum v;
+  float8 v = 0;
   int i;
 
   if (PG_ARGISNULL(0)) {
@@ -61,30 +58,10 @@ array_to_mean(PG_FUNCTION_ARGS)
       valsType != INT8OID &&
       valsType != FLOAT4OID &&
       valsType != FLOAT8OID) {
-    ereport(ERROR, (errmsg("Histogram subject must be SMALLINT, INTEGER, BIGINT, REAL, or DOUBLE PRECISION values")));
+    ereport(ERROR, (errmsg("Mean subject must be SMALLINT, INTEGER, BIGINT, REAL, or DOUBLE PRECISION values")));
   }
 
   valsLength = (ARR_DIMS(vals))[0];
-
-  switch (valsType) {
-    case INT2OID:
-      v.i16 = 0;
-      break;
-    case INT4OID:
-      v.i32 = 0;
-      break;
-    case INT8OID:
-      v.i64 = 0;
-      break;
-    case FLOAT4OID:
-      v.f4 = 0;
-      break;
-    case FLOAT8OID:
-      v.f8 = 0;
-      break;
-    default:
-      break;
-  }
 
   get_typlenbyvalalign(valsType, &valsTypeWidth, &valsTypeByValue, &valsTypeAlignmentCode);
 
@@ -92,27 +69,44 @@ array_to_mean(PG_FUNCTION_ARGS)
   deconstruct_array(vals, valsType, valsTypeWidth, valsTypeByValue, valsTypeAlignmentCode,
 &valsContent, &valsNullFlags, &valsLength);
 
-  // TODO: Iterate through the contents and sum things up.
-  
-  // Return the mean:
+  // Iterate through the contents and sum things up,
+  // then return the mean:
+  // Watch out for overflow:
+  // http://stackoverflow.com/questions/1930454/what-is-a-good-solution-for-calculating-an-average-where-the-sum-of-all-values-e/1934266#1934266
+
   switch (valsType) {
     case INT2OID:
-      PG_RETURN_INT16(v.i16 / valsLength);
+      for (i = 0; i < valsLength; i++) {
+        v += (DatumGetInt16(valsContent[i]) - v) / (i + 1);
+      }
+      PG_RETURN_INT16(v);
       break;
     case INT4OID:
-      PG_RETURN_INT32(v.i32 / valsLength);
+      for (i = 0; i < valsLength; i++) {
+        v += (DatumGetInt32(valsContent[i]) - v) / (i + 1);
+      }
+      PG_RETURN_INT32(v);
       break;
     case INT8OID:
-      PG_RETURN_INT64(v.i64 / valsLength);
+      for (i = 0; i < valsLength; i++) {
+        v += (DatumGetInt64(valsContent[i]) - v) / (i + 1);
+      }
+      PG_RETURN_INT64(v);
       break;
     case FLOAT4OID:
-      PG_RETURN_FLOAT4(v.f4 / valsLength);
+      for (i = 0; i < valsLength; i++) {
+        v += (DatumGetFloat4(valsContent[i]) - v) / (i + 1);
+      }
+      PG_RETURN_FLOAT4(v);
       break;
     case FLOAT8OID:
-      PG_RETURN_FLOAT8(v.f8 / valsLength);
+      for (i = 0; i < valsLength; i++) {
+        v += (DatumGetFloat8(valsContent[i]) - v) / (i + 1);
+      }
+      PG_RETURN_FLOAT8(v);
       break;
     default:
-      PG_RETURN_FLOAT8(0);
+      ereport(ERROR, (errmsg("Mean subject must be SMALLINT, INTEGER, BIGINT, REAL, or DOUBLE PRECISION values")));
       break;
   }
 }

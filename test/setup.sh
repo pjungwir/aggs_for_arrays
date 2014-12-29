cebc763c693f3610b982d9045bc7d972d1124024
Paul A. Jungwirth <pj@illuminatedcomputing.com>
2014-12-28 16:51:24 -0800

    Make sure we can run tests on a Mac with Postgres.app

--- test/setup.sh
+++ test/setup.sh
@@ -16,8 +16,14 @@ EOF
   exit 1;
 };
 
-sudo su - postgres -c "psql -d '$TESTING_DATABASE' -p '$TESTING_PORT' -c 'DROP EXTENSION IF EXISTS aggs_for_arrays'"
-sudo su - postgres -c "psql -d '$TESTING_DATABASE' -p '$TESTING_PORT' -c 'CREATE EXTENSION aggs_for_arrays'"
+if awk -F: '{ print $1 }' /etc/passwd | egrep '^postgres$'; then
+  sudo su - postgres -c "psql -d '$TESTING_DATABASE' -p '$TESTING_PORT' -c 'DROP EXTENSION IF EXISTS aggs_for_arrays'"
+  sudo su - postgres -c "psql -d '$TESTING_DATABASE' -p '$TESTING_PORT' -c 'CREATE EXTENSION aggs_for_arrays'"
+else
+  psql -d "$TESTING_DATABASE" -p "$TESTING_PORT" -c 'DROP EXTENSION IF EXISTS aggs_for_arrays'
+  psql -d "$TESTING_DATABASE" -p "$TESTING_PORT" -c 'CREATE EXTENSION aggs_for_arrays'
+fi
+
 
 psql --no-psqlrc -U "$TESTING_USER" -h "$TESTING_HOST" -p "$TESTING_PORT" "$TESTING_DATABASE" < test/setup.sql;
 

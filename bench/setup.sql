2d72cc5fd98c13798fdc6c3de4bfe19fcab6f09f
Paul A. Jungwirth <pj@illuminatedcomputing.com>
2014-12-28 16:52:39 -0800

    Add some more tables so we test the sorted_array_to_ functions

--- bench/setup.sql
+++ bench/setup.sql
@@ -7,10 +7,14 @@ DROP INDEX IF EXISTS idx_sample_groups_id;
 DROP INDEX IF EXISTS idx_sample_groups_measurement_id;
 DROP TABLE IF EXISTS samples;
 DROP TABLE IF EXISTS sample_groups;
+DROP TABLE IF EXISTS sorted_samples;
+DROP TABLE IF EXISTS sorted_sample_groups;
 DROP TABLE IF EXISTS measurements;
 DROP SEQUENCE IF EXISTS seq_measurements_id;
 DROP SEQUENCE IF EXISTS seq_samples_id;
 DROP SEQUENCE IF EXISTS seq_sample_groups_id;
+DROP SEQUENCE IF EXISTS seq_sorted_samples_id;
+DROP SEQUENCE IF EXISTS seq_sorted_sample_groups_id;
 
 CREATE SEQUENCE seq_measurements_id;
 CREATE TABLE measurements (
@@ -32,6 +36,20 @@ CREATE TABLE sample_groups (
   values FLOAT[] NOT NULL
 );
 
+CREATE SEQUENCE seq_sorted_samples_id;
+CREATE TABLE sorted_samples (
+  id INTEGER PRIMARY KEY DEFAULT nextval('seq_samples_id'),
+  measurement_id INTEGER NOT NULL,
+  value FLOAT NOT NULL
+);
+
+CREATE SEQUENCE seq_sorted_sample_groups_id;
+CREATE TABLE sorted_sample_groups (
+  id INTEGER PRIMARY KEY DEFAULT nextval('seq_sample_groups_id'),
+  measurement_id INTEGER NOT NULL,
+  values FLOAT[] NOT NULL
+);
+
 INSERT INTO measurements
 (name)
 SELECT  a::text
@@ -42,6 +60,7 @@ INSERT INTO samples
 (measurement_id, value)
 SELECT  m.id, random() * 20000000000 - 10000000000
 FROM    measurements m
+-- CROSS JOIN generate_series(1, 100)
 CROSS JOIN generate_series(1, 1000000)
 ;
 
@@ -52,11 +71,31 @@ FROM    samples
 GROUP BY measurement_id
 ;
 
+INSERT INTO sorted_samples
+(measurement_id, value)
+SELECT  measurement_id, value
+FROM    samples
+;
+
+INSERT INTO sorted_sample_groups
+(measurement_id, values)
+SELECT  measurement_id, array_agg(value)
+FROM    (SELECT measurement_id, value
+         FROM   samples
+         ORDER BY measurement_id, value) x
+GROUP BY measurement_id
+;
+
 CREATE INDEX idx_measurements_id ON measurements (id);
 CREATE INDEX idx_samples_id ON samples (id);
 CREATE INDEX idx_samples_measurement_id ON samples (measurement_id);
 CREATE INDEX idx_sample_groups_id ON sample_groups (id);
 CREATE INDEX idx_sample_groups_measurement_id ON sample_groups (measurement_id);
+CREATE INDEX idx_sorted_samples_id ON sorted_samples (id);
+CREATE INDEX idx_sorted_samples_measurement_id ON sorted_samples (measurement_id);
+CREATE INDEX idx_sorted_samples_value ON sorted_samples (measurement_id, value);
+CREATE INDEX idx_sorted_sample_groups_id ON sorted_sample_groups (id);
+CREATE INDEX idx_sorted_sample_groups_measurement_id ON sorted_sample_groups (measurement_id);
 
 COMMIT;
 

8ea8a90b835124d228b75742ae8ccab27941342b
Paul A. Jungwirth <pj@illuminatedcomputing.com>
2024-10-28 16:55:42 -0700

    more

--- test/defaults.sh
+++
@@ -1,6 +0,0 @@
-export TESTING_USER=${TESTING_USER:-aggs_for_arrays_test}
-export TESTING_PASSWORD=${TESTING_PASSWORD:-aggs_for_arrays_test}
-export TESTING_DATABASE=${TESTING_DATABASE:-aggs_for_arrays_test}
-export TESTING_HOST=${TESTING_HOST:-localhost}
-export TESTING_PORT=${TESTING_PORT:-5432}
-
--- test/test_helper.bash
+++
@@ -1,8 +0,0 @@
-
-source test/defaults.sh
-export PGPASSWORD="$TESTING_PASSWORD"
-
-function query() {
-  (echo "\\pset null 'NULL'"; echo $1) | psql --no-psqlrc --tuples-only -U "$TESTING_USER" -h "$TESTING_HOST" -p "$TESTING_PORT" "$TESTING_DATABASE" | grep -v 'Null display is' | sed 's/^ *//'
-}
-
